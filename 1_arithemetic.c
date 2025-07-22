6e715550f903ac9c7b3bec34613bfd1f792e9e1d
zayushiba07 <zayushiba07@gmail.com>
2025-07-22 10:36:46 +0530

    arithemetic op

---
+++ 1_arithemetic.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Enter the number1 :");
+    scanf("%d",&num1);
+    printf("Enter the number2 :");
+    scanf("%d",&num2);
+    printf("num1 is : %d \n num2 is : %d\n",num1,num2);
+    printf("Addition is : %d\n",num1+num2);
+    printf("substraction is : %d\n",num1-num2);
+    printf("multipliction is : %d\n",num1*num2);
+    printf("division is : %d\n",num1/num2);
+    printf("modulo is : %d\n",num1%num2);
+    printf("Increment is : %d\n",num1++);
+    printf("Decrement is : %d\n",num1--);
+    return 0;
+}
